3018ace505a7cc48c182852e501edd4fba67d5bb
Naoyuki Sogo <sogo@longtail-software.co.jp>
2025-10-11 19:09:19 +0900

    Refactor test scripts (#80)
    * separate settings to another file
    
    * refactor test scripts

--- server/test/release_test_local.sh
+++ server/test/release_test_local.sh
@@ -1,55 +1,10 @@
 #!/bin/bash
 TEST_DIR=$(cd $(dirname $0);pwd)
-pushd ${TEST_DIR}
-export WHEEL_CONFIG_DIR=$(mktemp -d tmp.XXXXXXXXXX)
-
-TAG_TEST_SERVER=wheel_release_test_server
-
-REMOTE_HOSTNAME=127.0.0.1
-REMOTE_PORT=4000
-KNOWN_HOSTS='['${REMOTE_HOSTNAME}']:'${REMOTE_PORT}
-NUM_JOB=5
-
-docker stop ${TAG_TEST_SERVER} 2>/dev/null
-echo "prepareing remotehost"
-{
-echo '[{'
-echo '  "name": "testServer",'
-echo '  "host": "'${REMOTE_HOSTNAME}'",'
-echo '  "path": "/home/testuser",'
-echo '  "username": "testuser",'
-echo '  "numJob": "'${NUM_JOB}'",'
-echo '  "port": '${REMOTE_PORT}','
-echo '  "id": "dummy-id",'
-echo '  "jobScheduler": "PBSPro",'
-echo '  "renewInterval": 0,'
-echo '  "renewDelay": 0,'
-echo '  "statusCheckInterval": 10,'
-echo '  "maxStatusCheckError": 10,'
-echo '  "readyTimeout": 5000'
-echo '}]'
-} > ${WHEEL_CONFIG_DIR}/remotehost.json
-
-echo boot up test server
-docker compose up ${TAG_TEST_SERVER} -d --wait --remove-orphans
-docker exec ${TAG_TEST_SERVER} /opt/pbs/bin/qmgr -c "set server job_history_enable=True"
-
-echo remove entry from known_hosts to avoid error if the entry already exists
-ssh-keygen -R ${KNOWN_HOSTS} 2>/dev/null
+./setup.sh
 
 echo "start UT"
-if [ x$1 == x-d ];then
-  export WHEEL_KEEP_FILES_AFTER_LAST_TEST=1
-fi
-
 WHEEL_TEST_REMOTEHOST=testServer WHEEL_TEST_REMOTE_PASSWORD=passw0rd npm run test
 rt=$?
 echo "UT finished"
-
-docker compose down
-rm -fr ${WHEEL_CONFIG_DIR}
-
-echo clean up known_hosts
-ssh-keygen -R ${KNOWN_HOSTS} 2>/dev/null
-
+./teardown.sh
 exit ${rt}
